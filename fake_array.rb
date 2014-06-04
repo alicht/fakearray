bb99b70c8b1df20c1274de646c9fcb43808f622f
Blake Johnson <blake41@gmail.com>
2014-06-04 13:59:41 -0400

    added solution

--- fake_array.rb
+++ fake_array.rb
@@ -1,7 +1,27 @@
 class FakeArray
+  # include Enumerable
+  attr_accessor :items
 
-  # you'll need a splat in this class somewhere
+  def initialize(*args)
+    @items = args
+  end
 
+  def [](index)
+    self.items[index]
+  end
 
+  def first
+    self.items[0]
+  end
+
+  def each
+    self.items.each do |element|
+      yield(element)
+    end
+  end
+
+  # def method_missing(method_name, *args, &block)
+  #   @items.send(method_name, *args, &block)
+  # end
 
 end
